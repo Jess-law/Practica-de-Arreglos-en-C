d94029de087ef3104b6024e6bcc94f7408530a44
Jessi.Soledad <82427901+Jess-law@users.noreply.github.com>
2022-06-07 21:18:43 -0300

    Add files via upload

---
+++ practica gral.c
@@ -0,0 +1,41 @@
+/*
+#define PI 3.1416
+
+///pasar de celsius a farenheit
+int main()
+{
+    int celsius=0;
+    int paseFarenheit=0;
+
+    printf("Ingrese grado celsius: \n");
+    scanf("%d", &celsius);
+
+    paseFarenheit = celsius + 30;
+
+    printf("El grado celsius ingresado equivale a %d grados farenheit\n", paseFarenheit);
+
+    return 0;
+}
+
+
+/// calcular longitud de circunferencia
+
+
+int main(){
+    float radio, circunferencia;
+
+    printf("Ingrese el valor del radio: \n");
+    scanf("%f", &radio);
+
+    circunferencia = PI * (pow(radio,2));
+
+    printf("La longitud de la circunferencia es: %.2f \n", circunferencia);
+
+
+
+
+
+
+return 0;
+}
+*/
